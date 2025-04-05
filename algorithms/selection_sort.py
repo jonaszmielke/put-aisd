01a9a87f9b6161876a38265f7b2075100f8cf3aa
Kabilon-hub <szymonkon2004@gmail.com>
2025-04-05 11:33:53 +0200

    Update selection_sort.py

--- algorithms/selection_sort.py
+++ algorithms/selection_sort.py
@@ -1,8 +1,25 @@
-def selection_sort():
+def selection_sort(arr):
+    n = len(arr)
+    
+    for i in range(n):
+        min_index = i
 
+        # Find the minimum element in the remaining unsorted array
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        # Swap the found minimum element with the first unsorted element
+        arr[i], arr[min_index] = arr[min_index], arr[i]
     return
 
 
 if __name__ == "__main__":
+    array = [5, 4, 1, 7, 0, 2, 3, 8]
+    print("Unsorted array:")
+    print(array)
 
-    selection_sort()
+    selection_sort(array)
+
+    print("Sorted array:")
+    print(array)
